94dc93b182afa3cfdb7469df3ee99e0f77bd9412
ArashPoorazam <arashpoorazam88@gmail.com>
2025-12-14 19:39:58 +0330

    add repl

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+		output := cleanInput(scanner.Text())[0]
+
+		fmt.Println("Your command was:", output)
+	}
 }
